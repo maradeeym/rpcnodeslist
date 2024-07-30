025896ff91b4e0eca6157c84a967b16581031be9
Martin <martin.kalliola@gmail.com>
2024-07-30 14:47:43 +0300

    updates

--- components/SimpleHero.js
+++ components/SimpleHero.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SimpleHero = () => {
   return (
-    <div className="p-14 bg-base-200">
+    <div className="py-14 px-6 bg-base-200">
       <div className="flex flex-wrap gap-4 max-w-screen-2xl justify-center mx-auto w-full">
-        <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
-            RPC Node Endpoints for Web3 developers
+        <h1 className="font-extrabold text-2xl sm:text-4xl lg:text-6xl tracking-tight md:-mb-4 text-center">
+            RPC Node Endpoints for < br/> Web3 developers
         </h1>
       </div>
     </div>
