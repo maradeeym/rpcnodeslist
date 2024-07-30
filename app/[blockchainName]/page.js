2245aeb8bc54a55e3314474931b0658ab2976065
Martin <martin.kalliola@gmail.com>
2024-07-30 20:14:57 +0300

    seo stuff, responsiveness for invidual pages

--- app/[blockchainName]/page.js
+++ app/[blockchainName]/page.js
@@ -1,84 +1,37 @@
-// app/blockchains/[blockchainName]/page.js
-"use client"
 import React from 'react';
-import { usePathname } from 'next/navigation';
 import blockchains from '@/app/rpcdb';
-import { Suspense } from 'react';
-import Header from '@/components/Header';
-import FAQ from '@/components/FAQ';
+import BlockchainDetails from '@/components/BlockchainDetails';
 
-const BlockchainDetails = () => {
-  const pathname = usePathname();
-  const blockchainName = decodeURIComponent(pathname.split('/').pop());
+export async function generateMetadata({ params }) {
+  const blockchainName = decodeURIComponent(params.blockchainName);
   const blockchain = blockchains.find(bc => bc.name.toLowerCase() === blockchainName.toLowerCase());
 
   if (!blockchain) {
-    return <p>Blockchain not found</p>;
+    return {
+      title: 'Blockchain not found',
+      description: 'The requested blockchain information could not be found.',
+    };
   }
 
+  return {
+    title: `${blockchain.name} RPC Endpoints | RPC Node List`,
+    description: `Find ${blockchain.name} Mainnet and Testnet RPC APIs for Web3 development.`,
+    keywords: `${blockchain.name} rpc, rpc node, rpc nodes,`,
+    alternates: {
+      canonical: `https://www.rpcnodelist.com/${blockchain.name}`
+    },
+  };
+}
+
+const Page = ({ params }) => {
+  const blockchainName = decodeURIComponent(params.blockchainName);
+  const blockchain = blockchains.find(bc => bc.name.toLowerCase() === blockchainName.toLowerCase());
+
   return (
-    <>
-    <Suspense>
-        <Header />
-    </Suspense>
-    <div className="p-6 bg-base-200 min-h-screen">
-      <div className="flex flex-col mb-8 bg-base-100 shadow-lg rounded-lg p-4 max-w-screen-2xl mx-auto w-full">
-        <h2 id={blockchain.name} className="text-2xl font-bold mb-4">
-          {blockchain.name}
-        </h2>
-        {blockchain.networks.map((network, idx) => (
-          <div key={idx} className="mb-6">
-            <h3 className="text-xl font-semibold mb-2">{network.network} RPCs</h3>
-            <table className="table-auto w-full mb-4 border-collapse border border-gray-300">
-              <thead>
-                <tr className="bg-gray-100">
-                  <th className="border px-4 py-2">Blockchain</th>
-                  <th className="border px-4 py-2">Network</th>
-                  <th className="border px-4 py-2">RPC URL</th>
-                  <th className="border px-4 py-2">Chain ID</th>
-                  <th className="border px-4 py-2">Native Currency</th>
-                  <th className="border px-4 py-2">Block Explorer</th>
-                </tr>
-              </thead>
-              <tbody>
-                {network.rpcUrls.map((url, urlIdx) => (
-                  <tr key={urlIdx} className="bg-green-50">
-                    <td className="border text-center px-4 py-2">{blockchain.name}</td>
-                    <td className="border text-center px-4 py-2">{network.network}</td>
-                    <td className="border px-4 py-2">
-                      <a
-                        href={url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500 hover:underline"
-                      >
-                        {url}
-                      </a>
-                    </td>
-                    <td className="border text-center px-4 py-2">{network.chainId}</td>
-                    <td className="border text-center px-4 py-2">{network.nativeCurrency}</td>
-                    <td className="border text-center px-4 py-2">
-                      <a
-                        href={network.blockExplorer}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500 hover:underline"
-                      >
-                        {network.blockExplorer}
-                      </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ))}
-      </div>
-    </div>
-    <FAQ />
-    </>
+    <BlockchainDetails blockchain={blockchain} />
   );
 };
 
-export default BlockchainDetails;
+export default Page;
+
 
