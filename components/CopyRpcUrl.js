1cb00b6359a6538720196469537bba97d4e31780
Martin <martin.kalliola@gmail.com>
2024-07-30 10:37:10 +0300

    copy button

---
+++ components/CopyRpcUrl.js
@@ -0,0 +1,37 @@
+// components/CopyRpcUrl.js
+"use client";
+import React, { useState } from 'react';
+
+const CopyRpcUrl = ({ url }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(url);
+    setShowModal(true);
+    setTimeout(() => {
+      setShowModal(false);
+    }, 1500);
+  };
+
+  return (
+    <div className="relative">
+      <div className="flex items-center justify-between">
+        <span>{url}</span>
+        <button
+          onClick={copyToClipboard}
+          className="btn btn-outline btn-sm mx-4"
+        >
+          Copy
+        </button>
+      </div>
+      {showModal && (
+        <div className="fixed top-4 left-1/2 transform -translate-x-1/2 p-2 bg-green-500 text-white text-sm rounded shadow-lg z-50">
+                  ✂ Copied!
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CopyRpcUrl;
+
--- components/RpcList.js
+++ components/RpcList.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import blockchains from '@/app/rpcdb';
+import CopyRpcUrl from './CopyRpcUrl';
+
 
 const RpcList = () => {
   return (
     <div className="p-6 bg-base-200 min-h-screen">
       {blockchains.map((blockchain, index) => (
         <div key={index} className="flex flex-col mb-8 bg-base-100 shadow-lg rounded-lg p-4 max-w-screen-2xl mx-auto w-full">
-          <h2 id={blockchain.name} className="text-2xl font-bold mb-4">{blockchain.name}</h2>
+          <h2 id={blockchain.name} className="text-2xl font-bold mb-4">{blockchain.name} RPC</h2>
           {blockchain.networks.map((network, idx) => (
             <div key={idx} className="mb-6">
-              <h3 className="text-xl font-semibold mb-2">{network.network} RPCs</h3>
+              <h3 className="text-xl font-semibold mb-2">{network.network}</h3>
               <table className="table-auto w-full mb-4 border-collapse border border-gray-300">
                 <thead>
                   <tr className="bg-gray-100">
@@ -26,16 +28,9 @@ const RpcList = () => {
                     <tr key={urlIdx} className="bg-green-50">
                       <td className="border text-center px-4 py-2">{blockchain.name}</td>
                       <td className="border text-center px-4 py-2">{network.network}</td>
-                      <td className="border px-4 py-2">
-                        <a
-                          href={url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-blue-500 hover:underline"
-                        >
-                          {url}
-                        </a>
-                      </td>
+                      <td className="border px-4 py-2 flex items-center justify-between">
+                      <CopyRpcUrl url={url} />
+                    </td>
                       <td className="border text-center px-4 py-2">{network.chainId}</td>
                       <td className="border text-center px-4 py-2">{network.nativeCurrency}</td>
                       <td className="border text-center px-4 py-2">
