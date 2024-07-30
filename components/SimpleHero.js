f09b77c847c79c3d015b418b5cc1f48258f24db9
Martin <martin.kalliola@gmail.com>
2024-07-30 07:10:37 +0300

    new headline

--- components/SimpleHero.js
+++ components/SimpleHero.js
@@ -4,7 +4,9 @@ const SimpleHero = () => {
   return (
     <div className="p-14 bg-base-200">
       <div className="flex flex-wrap gap-4 max-w-screen-2xl justify-center mx-auto w-full">
-        <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">RPC Node list for Web3 developers</h1>
+        <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
+            RPC Node Endpoints for Web3 developers
+        </h1>
       </div>
     </div>
   );
