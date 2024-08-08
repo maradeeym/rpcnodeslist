0c895ab59560a550a491c23d2749b286d90d9fa2
Martin <martin.kalliola@gmail.com>
2024-08-08 15:51:18 +0300

    sitemaps

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import PlausibleProvider from "next-plausible";
 import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
--- components/generateDynamicPaths.js
+++ components/generateDynamicPaths.js
@@ -2,10 +2,16 @@
 const blockchains = require('../app/rpcdb'); // Adjust the path as necessary
 
 const generateBlockchainPaths = () => {
-  return blockchains.map(blockchain => ({
-    loc: `/${blockchain.name.toLowerCase()}`,
-    lastmod: new Date().toISOString(), // or fetch actual lastmod if available
-  }));
+  return blockchains.map(blockchain => ([
+    {
+      loc: `/${blockchain.name.toLowerCase()}`,
+      lastmod: new Date().toISOString(), // or fetch actual lastmod if available
+    },
+    {
+      loc: `/add-to-wallet/${blockchain.name.toLowerCase()}`,
+      lastmod: new Date().toISOString(), // or fetch actual lastmod if available
+    }
+  ])).flat();
 };
 
 module.exports = generateBlockchainPaths;
