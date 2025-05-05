db0f1af5bd331a417f8087632be15e830cf998ac
Martin <martin.kalliola@gmail.com>
2025-05-05 14:07:48 +0300

    x

--- next.config.js
+++ next.config.js
@@ -1,3 +1,41 @@
+// Load the blockchains data
+const blockchains = require('./app/rpcdb');
+
+// Create redirects from old blockchain URLs to new network-specific URLs
+const redirects = async () => {
+  const redirectsArray = [];
+  
+  // For each blockchain, create a redirect from /[blockchainName] to /[blockchainName]-mainnet
+  // or to the first available network if mainnet doesn't exist
+  blockchains.forEach(blockchain => {
+    const blockchainSlug = blockchain.name.toLowerCase().replace(/ /g, '-');
+    
+    // First try to find mainnet
+    const mainnet = blockchain.networks.find(network => 
+      network.network.toLowerCase() === 'mainnet'
+    );
+    
+    if (mainnet) {
+      // Redirect to the mainnet network
+      redirectsArray.push({
+        source: `/${blockchain.name.toLowerCase().replace(/ /g, '-')}`,
+        destination: `/${blockchainSlug}-mainnet`,
+        permanent: true
+      });
+    } else if (blockchain.networks.length > 0) {
+      // If no mainnet, redirect to the first available network
+      const firstNetwork = blockchain.networks[0].network.toLowerCase().replace(/ /g, '-');
+      redirectsArray.push({
+        source: `/${blockchain.name.toLowerCase().replace(/ /g, '-')}`,
+        destination: `/${blockchainSlug}-${firstNetwork}`,
+        permanent: true
+      });
+    }
+  });
+  
+  return redirectsArray;
+};
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -9,6 +47,7 @@ const nextConfig = {
       "logos-world.net",
     ],
   },
+  redirects,
 };
 
 module.exports = nextConfig;
